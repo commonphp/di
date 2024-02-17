3c4a0eddf235f7346d3e5e2687ca0641e8f02e52
Timothy <timothy@commonphp.org>
2024-02-16 18:48:56 -0600

    Correct location to autoload.php in examples

--- examples/lookup-hooks.php
+++ examples/lookup-hooks.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 use CommonPHP\DependencyInjection\DependencyInjector;
 use CommonPHP\DependencyInjection\Support\ValueFinder;
